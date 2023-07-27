1a0f772534afeb619b6db563944a2c4aba48b4e2
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-07-27 15:56:44 -0300

    Big Bang

---
+++ README.md
@@ -0,0 +1,64 @@
+## Installation
+
+### Expo
+
+```shell
+expo install react-native-html-component react-native-webview
+```
+
+### Other
+
+```shell
+yarn install react-native-html-component react-native-webview
+```
+
+## Usage
+
+```jsx
+import ReactNativeHtmlComponent from 'react-native-html-component';
+
+export default function App() {
+    return (
+        <ReactNativeHtmlComponent html="<p>Hello World!</p>" />
+    )
+}
+```
+
+## Props
+
+### `html`
+
+➤ Type: **`string`** <br/>
+
+---
+
+### `allowTextSelection`
+
+➤ Type: **`boolean`** <br/>
+
+---
+
+### `backgroundColor`
+
+➤ Type: **`string`** <br/>
+
+---
+
+### `color`
+
+➤ Type: **`string`** <br/>
+
+---
+
+### `fontSize`
+
+➤ Type: **`number`** <br/>
+
+---
+
+### `style`
+
+➤ Type: **`StyleProp<ViewStyle>`** <br/>
+
+---
+
---
+++ lib/index.tsx
@@ -0,0 +1,65 @@
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
+import { useState } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+
+export type ReactNativeHtmlComponentProps = {
+  html: string;
+  allowTextSelection?: boolean;
+  backgroundColor?: string;
+  color?: string;
+  fontSize?: number;
+  style?: StyleProp<ViewStyle>;
+};
+
+export default function ReactNativeHtmlComponent({
+  html: body,
+  allowTextSelection,
+  color,
+  fontSize = 16,
+  style,
+}: ReactNativeHtmlComponentProps) {
+  const [height, setHeight] = useState<number>();
+
+  const script = `
+    <script>
+      function resize() {
+        window.ReactNativeWebView.postMessage(document.documentElement.scrollHeight);
+      }
+
+      document.addEventListener("DOMContentLoaded", () => {
+        resize();
+        (new ResizeObserver(resize)).observe(document.documentElement);
+      });
+    </script>
+  `;
+
+  return (
+    <WebView
+      onMessage={(e: WebViewMessageEvent) => setHeight(Number(e.nativeEvent?.data))}
+      style={[{ backgroundColor: 'transparent', height }, ...(Array.isArray(style) ? style : [style])]}
+      source={{
+        html: `
+          <html>
+            <head>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+
+              <style>
+                html {
+                  ${color ? `color: ${color};` : ''}
+                  font-size: ${fontSize}px;
+                  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                  ${allowTextSelection ? '' : 'user-select: none;'}
+                }
+              </style>
+
+              ${script}
+            </head>
+            <body>
+              ${body}
+            </body>
+          </html>
+        `,
+      }}
+    />
+  );
+}
